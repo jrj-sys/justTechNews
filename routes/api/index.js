a29ed40fdf035d6d0cce8511a2d9450b5942554b
jrj-sys <jamesjamel99@gmail.com>
2022-04-27 20:26:53 -0500

    Post model and manipulation

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user_routes.js');
+const postRoutes = require('./post_routes');
+
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
